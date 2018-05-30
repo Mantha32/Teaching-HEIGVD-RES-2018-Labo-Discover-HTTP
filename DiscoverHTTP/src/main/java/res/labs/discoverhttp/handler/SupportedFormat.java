073de65e5df8235ea606fa6b649db04d9757e6c8
Iando Rafid <iando.rafid@gmail.com>
2018-05-30 23:46:19 +0200

    add supported format

---
+++ DiscoverHTTP/src/main/java/res/labs/discoverhttp/handler/SupportedFormat.java
@@ -0,0 +1,11 @@
+package res.labs.discoverhttp.handler;
+
+/**
+ *
+ * @author fidimala
+ */
+public class SupportedFormat {
+    public static String HTML = "txt/html";
+    public static String XML = "application/xml";
+    public static String JSON = "application/json";
+}
